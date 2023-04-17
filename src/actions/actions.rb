9bde8218381ea32d75b933b86d66c55997521864
Julian Leon <juliandgleon@gmail.com>
2023-04-17 18:30:10 -0500

    Re-structuring of the project

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -1,133 +1,62 @@
-require_relative '../models/state'
-
 module Actions
-  # Access Control
-  private :cal_next_position, :position_is_valid?, :move_to, :end_game
-
-  def self.move_snake( state )
+  def self.move_snake(state)
     next_direction = state.next_direction
-    next_position = cal_next_position( state )
-
-    if position_is_valid?( state, next_position )
-      # ...
-      move_to( state, next_position )
+    next_position = calc_next_position(state)
+    # verificar que la siguiente cassilla sea valida
+    if position_is_valid?(state, next_position)
+      move_snake_to(state, next_position)
     else
-      end_game( state )
-      # ...
+      end_game(state)
     end
   end
 
-  def cal_next_position( state )
-    current_position = state.snake.positions.first
-    new_position = nil
-    move_speed = 2
-    @x_speed = 0
-    @y_speed = 0
-
-    # Clickboard
-    on :key_down do |event|
-      puts event.type
-
-      case event.key
-        when 'd', 'right' then # x
-          @x_speed = 2
-          @y_speed = 0
-
-          new_position = Model::Coord.new(
-            current_position.x_position + move_speed,
-            current_position.y_position
-          )
-        when 'a', 'left' then
-          @x_speed = -2
-          @y_speed = 0
-
-          new_position = Model::Coord.new(
-            current_position.x_position - move_speed,
-            current_position.y_position
-          )
-        when 'w', 'up' then # y
-          @y_speed = -2
-          @x_speed = 0
-
-          new_position = Model::Coord.new(
-            current_position.x_position,
-            current_position.y_position - move_speed
-          )
-        when 's', 'down' then
-          @y_speed = 2
-          @x_speed = 0
-
-          new_position = Model::Coord.new(
-            current_position.x_position,
-            current_position.y_position + move_speed
-          )
-      end
-    end
-
-    new_position
-  end
-
-  def position_is_valid?( state, next_position )
-    is_valid = true
-
-    if ( (next_position.x_position >= state.grid.rows) || (next_position.x_position < 0) )
-      is_valid = false
-    end
-
-    if ( (next_position.y_position >= state.grid.cols) || (next_position.y_position < 0) )
-      is_valid = false
-    end
-
-    # Check that the snake does not overlap
-    if is_valid
-      return !(state.snake.positions.include? next_position)
-    else
-      return false
+  private
+  
+  def self.calc_next_position(state)
+    curr_position = state.snake.positions.first
+    case state.next_direction
+    when Model::Direction::UP
+      # decrementar fila
+      return Model::Coord.new(
+          curr_position.row - 1, 
+          curr_position.col)
+    when Model::Direction::RIGHT
+      # incrementar col
+      return Model::Coord.new(
+          curr_position.row, 
+          curr_position.col + 1)
+    when Model::Direction::DOWN
+      # incrementar fila
+      return Model::Coord.new(
+          curr_position.row + 1,
+          curr_position.col)
+    when Model::Direction::LEFT
+      # decrementar col
+      return Model::Coord.new(
+          curr_position.row, 
+          curr_position.col - 1)
     end
   end
 
-  def move_to( state, next_position )
-    new_positions = [ next_position ] + state.snake.positions[0...-1]
+  def self.position_is_valid?(state, position)
+    # verificar q este en la grilla
+    is_invalid = ((position.row >= state.grid.rows ||
+      position.row < 0) || 
+      (position.col >= state.grid.cols ||
+      position.col < 0))
+    return false if is_invalid
+    # verificar q no este superponiendo a la serpiente
+    return !(state.snake.positions.include? position)
+  end
+
+  def self.move_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions[0...-1]
     state.snake.positions = new_positions
     state
   end
 
-  def end_game( state )
+  def self.end_game(state)
     state.game_finished = true
     state
   end
 end
-
-=begin
-  def self.cal_next_position( state )
-    current_position = state.snake.positions.first
-    new_position = nil
-    move_speed = 2
-    @x_speed = 0
-    @y_speed = 0
-
-    # Clickboard
-    on :key_down do |event|
-      puts event.type
-
-      case event.key
-        when 'd', 'right' then # x
-          @x_speed = 2
-          @y_speed = 0
-        when 'a', 'left' then
-          @x_speed = -2
-          @y_speed = 0
-        when 'w', 'up' then # y
-          @y_speed = -2
-          @x_speed = 0
-        when 's', 'down' then
-          @y_speed = 2
-          @x_speed = 0
-      end
-
-      # state.snake.positions
-
-      state.snake.x += @x_speed
-      state.snake.y += @y_speed
-    end
-=end
