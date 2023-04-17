cf494c76d8a97f457a851d5c547763ec7163f4e2
Julian Leon <juliandgleon@gmail.com>
2023-04-17 10:43:38 -0500

    Configs file added

---
+++ configs.rb
@@ -0,0 +1,11 @@
+module Configs
+  CONSOLE_CONFIGS = {
+    "title" => "Example Julian!",
+    "background" => "blue",
+    "diagnostics" => true,
+    "resizable" => true,
+    "pixel_size" => 20,
+    "rows" => 30,
+    "cols" => 42
+  }
+end
