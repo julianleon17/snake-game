d314d81c8988d56153c5b3f0decf601d61f7042a
Julian Leon <juliandgleon@gmail.com>
2023-04-18 17:32:38 -0500

    Logic for the next direction added

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -1,61 +1,86 @@
 module Actions
-  def self.move_snake(state)
-    next_direction = state.next_direction
-    next_position = calc_next_position(state)
-    # verificar que la siguiente cassilla sea valida
-    if position_is_valid?(state, next_position)
-      move_snake_to(state, next_position)
+  def self.move_snake( state )
+    next_direction = state.current_direction
+    next_position = calc_next_position( state )
+
+    # Next position is valid?
+    if position_is_valid?( state, next_position )
+      move_snake_to( state, next_position )
     else
-      end_game(state)
+      end_game( state )
     end
   end
 
   private
-  
-  def self.calc_next_position(state)
-    curr_position = state.snake.positions.first
-    case state.next_direction
-    when Model::Direction::UP
-      # decrementar fila
-      return Model::Coord.new(
-          curr_position.row - 1, 
-          curr_position.col)
-    when Model::Direction::RIGHT
-      # incrementar col
-      return Model::Coord.new(
-          curr_position.row, 
-          curr_position.col + 1)
-    when Model::Direction::DOWN
-      # incrementar fila
-      return Model::Coord.new(
-          curr_position.row + 1,
-          curr_position.col)
-    when Model::Direction::LEFT
-      # decrementar col
-      return Model::Coord.new(
-          curr_position.row, 
-          curr_position.col - 1)
+
+  def self.calc_next_position( state )
+    current_position = state.snake.positions.first
+
+    case state.current_direction
+      when Model::Direction::UP
+        return Model::Coord.new(
+          current_position.row - 1, 
+          current_position.col
+        )
+      when Model::Direction::RIGHT
+        return Model::Coord.new(
+          current_position.row, 
+          current_position.col + 1
+        )
+      when Model::Direction::DOWN
+        return Model::Coord.new(
+          current_position.row + 1,
+          current_position.col
+        )
+      when Model::Direction::LEFT
+        return Model::Coord.new(
+          current_position.row, 
+          current_position.col - 1
+        )
     end
   end
 
-  def self.position_is_valid?(state, position)
+  def self.position_is_valid?( state, position )
     # verificar q este en la grilla
-    is_invalid = ((position.row >= state.grid.rows ||
-      position.row < 0) || 
-      (position.col >= state.grid.cols ||
-      position.col < 0))
+    is_invalid = ( (position.row >= state.grid.rows || position.row < 0) || (position.col >= state.grid.cols || position.col < 0) )
     return false if is_invalid
     # verificar q no este superponiendo a la serpiente
     return !(state.snake.positions.include? position)
   end
 
-  def self.move_snake_to(state, next_position)
-    new_positions = [next_position] + state.snake.positions[0...-1]
+  def self.move_snake_to( state, next_position )
+    new_positions = [ next_position ] + state.snake.positions[0...-1]
     state.snake.positions = new_positions
     state
   end
 
-  def self.end_game(state)
+  def self.change_direction( state, direction )
+    if next_direction_is_valid?( state, direction )
+      state.current_direction = direction
+      puts 'Valid direction!'
+    else
+      puts 'Invalid direction!'
+    end
+
+    state
+  end
+
+  def self.next_direction_is_valid?(  state, direction  )
+    case state.current_direction
+      when Model::Direction::UP
+        return true if direction != Model::Direction::DOWN
+      when Model::Direction::DOWN
+        return true if direction != Model::Direction::UP
+      when Model::Direction::LEFT
+        return true if direction != Model::Direction::RIGHT
+      when Model::Direction::RIGHT
+        return true if direction != Model::Direction::LEFT
+    end
+
+    return false
+  end
+
+  def self.end_game( state )
     state.game_finished = true
     state
   end
--- src/model/state.rb
+++ src/model/state.rb
@@ -1,4 +1,8 @@
+require_relative "../../configs"
+
 module Model
+  CONFIGS = Configs::CONSOLE_CONFIGS
+
   module Direction
     UP = :up
     RIGHT = :right
@@ -6,31 +10,50 @@ module Model
     LEFT = :left
   end
 
-  class Coord < Struct.new(:row, :col)
+  class Coord < Struct.new( :row, :col )
   end
 
   class Food < Coord
   end
 
-  class Snake < Struct.new(:positions)
+  class Snake < Struct.new( :positions )
   end
 
-  class Grid < Struct.new(:rows, :cols)
+  class Grid < Struct.new( :rows, :cols )
   end
 
-  class State < Struct.new(:snake, :food, :grid, :next_direction, :game_finished)
+  class State < Struct.new( :snake, :food, :grid, :current_direction, :game_finished )
   end
 
   def self.initial_state
-    Model::State.new(
-        Model::Snake.new([
-            Model::Coord.new(1,1),
-            Model::Coord.new(0,1)
-        ]),
-        Model::Food.new(4, 4),
-        Model::Grid.new(8, 12),
-        Direction::DOWN,
-        false
+    # Grid
+    grid = Model::Grid.new( CONFIGS[ "rows" ], CONFIGS[ "cols" ] )
+
+    # Food
+    food = Model::Food.new(
+      rand( CONFIGS[ "rows" ] ),
+      rand( CONFIGS[ "cols" ] )
     )
+
+    # Snake
+    snake = Model::Snake.new( [
+      Model::Coord.new( 1, 1 ),
+      Model::Coord.new( 0, 1 )
+    ] )
+
+    # Next Direction
+    current_direction = Direction::DOWN
+
+    # Model::State.new(
+    #     Model::Snake.new([
+    #         Model::Coord.new(1,1),
+    #         Model::Coord.new(0,1)
+    #     ]),
+    #     Model::Food.new(4, 4),
+    #     Model::Grid.new(8, 12),
+    #     Direction::DOWN,
+    #     false
+    # )
+    Model::State.new( snake, food, grid, current_direction, false )
   end
 end
