52ee7689c31eebe2d677fb690742af8404c688ae
Julian Leon <juliandgleon@gmail.com>
2023-04-21 11:11:45 -0500

    Test to grow the snake and change food position added

--- test/test.rb
+++ test/test.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "minitest/mock"
 require_relative "../src/actions/actions"
 require_relative "../src/model/state"
 
@@ -6,8 +7,8 @@ class ActionsTest < Minitest::Test
   def setup
     @initial_state = Model::State.new(
       Model::Snake.new([
-          Model::Coord.new(1,1),
-          Model::Coord.new(0,1)
+        Model::Coord.new(1,1),
+        Model::Coord.new(0,1)
       ]),
       Model::Food.new(4, 4),
       Model::Grid.new(8, 12),
@@ -19,8 +20,8 @@ class ActionsTest < Minitest::Test
   def test_move_snake
     expected_state = Model::State.new(
       Model::Snake.new( [
-          Model::Coord.new( 2,1 ),
-          Model::Coord.new( 1,1 )
+        Model::Coord.new( 2,1 ),
+        Model::Coord.new( 1,1 )
       ] ),
       Model::Food.new( 4, 4 ),
       Model::Grid.new( 8, 12 ),
@@ -35,8 +36,8 @@ class ActionsTest < Minitest::Test
   def test_change_direction_invalid
     expected_state = Model::State.new(
       Model::Snake.new([
-          Model::Coord.new(1,1),
-          Model::Coord.new(0,1)
+        Model::Coord.new(1,1),
+        Model::Coord.new(0,1)
       ]),
       Model::Food.new(4, 4),
       Model::Grid.new(8, 12),
@@ -51,8 +52,8 @@ class ActionsTest < Minitest::Test
   def test_change_direction_valid
     expected_state = Model::State.new(
       Model::Snake.new([
-          Model::Coord.new(1,1),
-          Model::Coord.new(0,1)
+        Model::Coord.new(1,1),
+        Model::Coord.new(0,1)
       ]),
       Model::Food.new(4, 4),
       Model::Grid.new(8, 12),
@@ -62,5 +63,70 @@ class ActionsTest < Minitest::Test
 
     actual_state = Actions::change_direction( @initial_state, Model::Direction::LEFT )
     assert_equal actual_state, expected_state
+  end
+
+  # Test Food
+  def test_snake_grow
+    initial_state = Model::State.new(
+      Model::Snake.new([
+        Model::Coord.new(1,1),
+        Model::Coord.new(0,1)
+      ]),
+      Model::Food.new(2, 1),
+      Model::Grid.new(8, 12),
+      Model::Direction::DOWN,
+      false
+    )
+
+    expected_state = Model::State.new(
+      Model::Snake.new( [
+        Model::Coord.new( 2,1 ),
+        Model::Coord.new( 1,1 ),
+        Model::Coord.new( 0,1 )
+      ] ),
+      nil,
+      Model::Grid.new( 8, 12 ),
+      Model::Direction::DOWN,
+      false
+    )
+
+    actual_state = Actions::move_snake( initial_state )
+    assert_equal( actual_state.snake.positions, [
+      Model::Coord.new( 2,1 ),
+      Model::Coord.new( 1,1 ),
+      Model::Coord.new( 0,1 )
+    ] )
+  end
+
+  def test_generate_food
+    initial_state = Model::State.new(
+      Model::Snake.new([
+        Model::Coord.new(1,1),
+        Model::Coord.new(0,1)
+      ]),
+      Model::Food.new( 2, 1 ),
+      Model::Grid.new( 8, 12 ),
+      Model::Direction::DOWN,
+      false
+    )
+
+    expected_state = Model::State.new(
+      Model::Snake.new( [
+        Model::Coord.new( 2,1 ),
+        Model::Coord.new( 1,1 ),
+        Model::Coord.new( 0,1 )
+      ] ),
+      Model::Food.new( 0, 0 ),
+      Model::Grid.new( 8, 12 ),
+      Model::Direction::DOWN,
+      false
+    )
+
+      # rand( CONFIGS[ "rows" ] ),
+      # rand( CONFIGS[ "cols" ] )
+      Actions.stub( :rand, 0 ) do
+        actual_state = Actions::move_snake( initial_state )
+        assert_equal actual_state, expected_state
+      end
   end
 end
